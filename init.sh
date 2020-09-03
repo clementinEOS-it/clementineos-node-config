d0b1f2c80945ab062a6af0f62ea74c5cb926f719
Giuseppe Zileni <6775950+gzileni@users.noreply.github.com>
2020-09-03 19:12:33 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo mkdir /opt/eosio && cd /opt/eosio && sudo mkdir nodeos wallet scripts
+
+cd /opt/eosio/nodeos && sudo mkdir config data
+cd /opt/eosio/wallet && sudo mkdir config data keys
+
