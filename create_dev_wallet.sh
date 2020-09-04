dbd854e9bf728cf813bc98c27fded70c2025a551
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2020-09-04 10:06:17 +0200

    update scripts

--- create_dev_wallet.sh
+++ create_dev_wallet.sh
@@ -4,8 +4,8 @@ echo 'create default wallet'
 cleos wallet create --file ~/wallet_password.txt
 
 echo 'unlock default wallet'
-pwd='sudo cat ~/wallet_password.txt'
-cleos wallet unlock --password $pwd
+password=$(cat ~/wallet_password.txt)
+cleos wallet unlock --password $password
 
 echo 'create development account'
 cleos wallet import --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
@@ -14,4 +14,4 @@ echo 'create public/private keys to system accounts'
 cleos wallet create_key
 
 echo 'show keys ...'
-cleos wallet private_keys --password $pwd
+cleos wallet private_keys --password $password
