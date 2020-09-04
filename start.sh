7de9d0e7e839a2768a61e96bbd7f7855c5206d89
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2020-09-04 09:29:35 +0200

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo 'starting nodeos ...'
-sudo /opt/eosio/scripts/nodeosd_start.sh && sudo /opt/eosio/scripts/keosd_start.sh
+sudo /opt/eosio/scripts/nodeosd_start.sh
+
+echo 'starting wallet ...'
+sudo /opt/eosio/scripts/keosd_start.sh
